68008678c57c5e383e57615d7656f323b15b7688
Alessandro <alessandrocosta103@gmail.com>
2024-03-20 16:19:50 -0300

    finaliza funções importante do projeto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-function criptografar(){
+function criptografar(texto){
     var texto = document.getElementById("textarea").value;
-    
+       texto = texto.replaceAll("e", "enter");
+       texto = texto.replaceAll("i", "imes");
+       texto = texto.replaceAll("a", "ai"); 
+       texto = texto.replaceAll("o", "ober"); 
+       texto = texto.replaceAll("u", "ufat"); 
+       document.getElementById("ladoDireito").style.visibility = "none";
+       var campo = document.getElementById("conteudo");
+       campo.innerHTML = texto;
 }
 
-function descriptografar(){
-
+function descriptografar(texto){
+    var texto = document.getElementById("textarea").value;
+       texto = texto.replaceAll("enter", "e");
+       texto = texto.replaceAll("imes", "i");
+       texto = texto.replaceAll("ai", "a"); 
+       texto = texto.replaceAll("ober", "o"); 
+       texto = texto.replaceAll("ufat", "u"); 
+       document.getElementById("ladoDireito").style.visibility = "none";
+       var campo = document.getElementById("conteudo");
+       campo.innerHTML = texto;
 }
+
