3cf5576d07c457dca7060a4042f38bd6dbdbf370
Alessandro <alessandrocosta103@gmail.com>
2024-05-09 17:17:07 -0300

    finaliza interface desktop

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-document.getElementById("botaoCopiar").style.visibility = "hidden";
-document.getElementById("limpar").style.visibility = "hidden";
+document.getElementById("botaoDescript").setAttribute('disabled',true);
+document.getElementById("botaoCopiar").style.display = "none";
+document.getElementById("limpar").style.display = "none";
 function temAcentoOuEspecial(texto) {
     var texto = document.getElementById("textarea").value;
     var acentosEspeciais = "áàãâäéèêëíìîïóòõôöúùûüçñÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇÑ!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?1234567890";
@@ -11,7 +12,7 @@ function temAcentoOuEspecial(texto) {
     return false;
 }
 function criptografar() {
-    var texto = document.getElementById("textarea").value;
+    var texto = document.getElementById("textarea").value.toString();
     var v = temAcentoOuEspecial();
     if (v == true) {
         let test = document.querySelector('h6');
@@ -24,6 +25,7 @@ function criptografar() {
         let test = document.querySelector('h6');
         test.style.color = "red";
     } else {
+        document.getElementById("botaoDescript").removeAttribute('disabled');
         let test = document.querySelector('h6');
         test.style.color = "#65747D";
         texto = texto.replaceAll("e", "enter");
@@ -31,15 +33,16 @@ function criptografar() {
         texto = texto.replaceAll("a", "ai");
         texto = texto.replaceAll("o", "ober");
         texto = texto.replaceAll("u", "ufat");
-        document.getElementById("campo").style.visibility = "none";
-        document.getElementById("botaoCopiar").style.visibility = "visible";
-        document.getElementById("limpar").style.visibility = "visible";
-        var campo = document.getElementById("conteudo");
-        campo.innerHTML = texto;
+        document.getElementById("campo").style.display = "none";
+        document.getElementById("botaoCopiar").style.display = "block";
+        document.getElementById("limpar").style.display = "block";
+        let campo = document.querySelector("#conteudo");
+        campo.innerHTML = `${texto}`;
+        console.log(texto);
     }
 }
 function descriptografar(texto) {
-    var texto = document.getElementById("textarea").value;
+    var texto = document.getElementById("textarea").value.toString();
     var v = temAcentoOuEspecial();
     if (v == true) {
         let test = document.querySelector('h6');
@@ -59,10 +62,11 @@ function descriptografar(texto) {
         texto = texto.replaceAll("ai", "a");
         texto = texto.replaceAll("ober", "o");
         texto = texto.replaceAll("ufat", "u");
-        document.getElementById("botaoCopiar").style.visibility = "visible";
-        document.getElementById("limpar").style.visibility = "visible";
-        var campo = document.getElementById("conteudo");
-        campo.innerHTML = texto;
+        document.getElementById("campo").style.display = "none";
+        document.getElementById("botaoCopiar").style.display = "block";
+        document.getElementById("limpar").style.display = "block";
+        var campo = document.querySelector("#conteudo");
+        campo.innerHTML = `${texto}`;
     }
 }
 function copiar() {
@@ -71,10 +75,12 @@ function copiar() {
 }
 document.querySelector("#botaoCopiar").addEventListener("click", copiar);
 function limpar() {
+    document.getElementById("botaoDescript").setAttribute('disabled',true);
     campoTextarea = document.querySelector('textarea');
     campoTextarea.value = "";
-    document.getElementById("botaoCopiar").style.visibility = "hidden";
-    document.getElementById("limpar").style.visibility = "hidden";
+    document.getElementById("campo").style.display = "block";
+    document.getElementById("botaoCopiar").style.display = "none";
+    document.getElementById("limpar").style.display = "none";
     let campoConteudo = document.querySelector("#conteudo");
-    campoConteudo.innerHTML = "Digite um texto que você deseja Criptografar ou Descriptografar.";
+    campoConteudo.innerHTML = "Digite um texto que você deseja Criptografar ou Descriptografar";
 }
